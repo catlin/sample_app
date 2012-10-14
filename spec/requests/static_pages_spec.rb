a74080e36c7c1901604590a711516d8f810722cd
Andy Catlin <andycatlin@hotmail.com>
2012-10-14 14:12:09 -0400

    finish chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,18 @@ describe "Home page" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title Finer Refiner" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                    :text => "#{base_title} | Home")
+                    :text => "Finer Refiner")
     end
+
+     it "should have not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                    :text => "| Home")
+    end
+
   end
 
 describe "Help page" do
